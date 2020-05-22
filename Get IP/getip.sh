6316b61418ee141bb7cbbe981e30935ca6e7ee4f
bhmth <bhmth@infernal>
2020-05-22 03:33:51 +0200

    Initial Version

--- Get IP/getip.sh
+++ Get IP/getip.sh
@@ -4,9 +4,9 @@ interface_status=$(cat /sys/class/net/$1/operstate)
 if echo $interface_status | grep -q "down"; then
 	echo -e "$1 is \e[31mDOWN\e[0m"
 elif echo $interface_status | grep -q "up" || echo $interface_status | grep -q "unknown"; then
-	vari=$(ifconfig $1 | grep inet | head -n 1 | cut -d ' ' -f 10)
-	echo $vari | xclip -selection c
-	echo -e "\e[32m$vari"
+	ip=$(ifconfig $1 | grep inet | head -n 1 | cut -d ' ' -f 10)
+	echo $ip | xclip -selection c
+	echo -e "\e[32m$ip"
 else
 	echo -e "USAGE: \e[91m./wlan0.sh interface"
 fi
