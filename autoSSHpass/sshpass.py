cd9881f12c5290637b8330a86aeff72f28c1474e
bhmth <bhmth@infernal>
2020-05-26 04:42:13 +0200

    SSHPASS auto

---
+++ autoSSHpass/sshpass.py
@@ -0,0 +1,15 @@
+import sys
+import os
+from colorama import Fore, Back, Style
+
+username = sys.argv[1]
+password = sys.argv[2]
+ip = sys.argv[3]
+
+while True:
+    banner = '╭─[{}{}@{}{}]\n'.format(Fore.GREEN,username,ip,Fore.WHITE)
+    shell = input('{}╰─>'.format(banner))
+    if shell == "exit":
+        break
+    else:
+        os.system("sshpass -p {} ssh {}@{} {}".format(password, username, ip, shell))
